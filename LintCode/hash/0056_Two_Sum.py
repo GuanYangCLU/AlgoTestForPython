a405d329eda4fa93108f9a05f303a59a5a063dcd
Guan <33585352+GuanYangCLU@users.noreply.github.com>
2020-07-18 16:31:02 -0700

    Update 0056_Two_Sum.py

--- LintCode/hash/0056_Two_Sum.py
+++ LintCode/hash/0056_Two_Sum.py
@@ -13,3 +13,28 @@ class Solution:
                 return [numbers.index(cp), i]
             hashSet.add(cp)
         return [-1, -1]
+
+    # Two Pointer:
+class Solution:
+    """
+    @param numbers: An array of Integer
+    @param target: target = numbers[index1] + numbers[index2]
+    @return: [index1, index2] (index1 < index2)
+    """
+    def twoSum(self, numbers, target):
+        # write your code here
+        if not numbers:
+            return [-1, -1]
+        sortedNums = sorted([ (number, index)
+            for index, number in enumerate(numbers)
+        ])
+        left, right = 0, len(sortedNums) - 1
+        while left < right:
+            if sortedNums[left][0] + sortedNums[right][0] == target:
+                return sorted([sortedNums[left][1], sortedNums[right][1]])
+            if sortedNums[left][0] + sortedNums[right][0] > target:
+                right -= 1
+                continue
+            if sortedNums[left][0] + sortedNums[right][0] < target:
+                left += 1
+        return [-1, -1]
