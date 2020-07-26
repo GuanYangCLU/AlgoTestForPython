64465d3be12f35ab4281b59fe6960831eba2c897
Guan <33585352+GuanYangCLU@users.noreply.github.com>
2020-07-26 14:38:51 -0700

    Create 0183_Wood_Cut.py

---
+++ LintCode/ladder/chapter_3/0183_Wood_Cut.py
@@ -0,0 +1,26 @@
+class Solution:
+    """
+    @param L: Given n pieces of wood with length L[i]
+    @param k: An integer
+    @return: The maximum length of the small pieces
+    """
+    def woodCut(self, L, k):
+        # write your code here
+        # when k = 1, size cannot gt maxL
+        if not L:
+            return 0
+        size = min(max(L), sum(L) // k)
+        
+        left, right = 1, size
+        while left + 1 < right:
+            mid = (left + right) // 2
+            if self.getKBySize(L, mid) < k:
+                right = mid
+                continue
+            if self.getKBySize(L, mid) >= k:
+                left = mid
+        
+        return right if self.getKBySize(L, left) < k else left
+        
+    def getKBySize(self, L, size):
+        return sum([n // size for n in L])
