971122f897a85be282f20ed6b2bd2140cd75a4a2
Guan <33585352+GuanYangCLU@users.noreply.github.com>
2020-07-27 23:55:05 -0700

    Update 0069_Binary_Tree_Level_Order_Traversal.py

--- LintCode/bfs/0069_Binary_Tree_Level_Order_Traversal.py
+++ LintCode/bfs/0069_Binary_Tree_Level_Order_Traversal.py
@@ -1,3 +1,29 @@
+# single queue
+class Solution:
+    """
+    @param root: A Tree
+    @return: Level order a list of lists of integer
+    """
+    def levelOrder(self, root):
+        # write your code here
+        if not root:
+            return []
+        
+        queue = collections.deque([root])
+        res = []
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                level.append(node.val)
+            res.append(level)
+        return res
+
+# double queue
 """
 Definition of TreeNode:
 class TreeNode:
@@ -31,3 +57,6 @@ class Solution:
             res.append(level)
 
         return res
+    
+# dummy node
+
