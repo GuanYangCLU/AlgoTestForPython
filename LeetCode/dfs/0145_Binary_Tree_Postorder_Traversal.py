f1d907395d54ea0b43e9683e26ee973102180bfa
Guan <33585352+GuanYangCLU@users.noreply.github.com>
2021-01-26 01:18:51 -0800

    Create 0145_Binary_Tree_Postorder_Traversal.py

---
+++ LeetCode/dfs/0145_Binary_Tree_Postorder_Traversal.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        self.dfs(root, res)
+        return res
+        
+    def dfs(self, root, res):
+        if not root:
+            return
+        self.dfs(root.left, res)
+        self.dfs(root.right, res)
+        res.append(root.val)
