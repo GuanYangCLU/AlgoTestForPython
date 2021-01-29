94c47da55f30715ad38e92d4779275d15d6b2997
Guan <33585352+GuanYangCLU@users.noreply.github.com>
2021-01-29 00:25:01 -0800

    Create 1369_Most_Common_Word.py

---
+++ LintCode/FLAG/Amazon/1369_Most_Common_Word.py
@@ -0,0 +1,25 @@
+# Lint Amazon Ladder 1
+import re
+
+class Solution:
+    """
+    @param paragraph: 
+    @param banned: 
+    @return: nothing
+    """
+    def mostCommonWord(self, paragraph, banned):
+        # refer the one line return
+        wordList = re.findall(r'\w+', paragraph.lower())
+        banSet = set(banned)
+        # return collections.Counter(w for w in wordList if w not in banSet).most_common(1)[0][0]
+        wordDict = {}
+        mostWord, freq = '', 0
+        for word in wordList:
+            if word in banSet:
+                continue
+            curWordFreq = wordDict.get(word, 0) + 1
+            wordDict[word] = curWordFreq
+            if curWordFreq > freq:
+                mostWord, freq = word, curWordFreq
+        return mostWord
+        
