c58e29d24daf98c227b6b424c6f696a31d7f1e25
Guan <33585352+GuanYangCLU@users.noreply.github.com>
2021-02-02 23:58:30 -0800

    Create 0020_Valid_Parentheses.py

---
+++ LeetCode/arrayAndString/0020_Valid_Parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    """
+    @param s: A string
+    @return: whether the string is a valid parentheses
+    """
+    def isValidParentheses(self, s):
+        # write your code here
+        pushed = []
+        pairs = { ')': '(', ']': '[', '}': '{' }
+        for c in s:
+            if not c in pairs:
+                pushed.append(c)
+                continue
+            if not pushed or pairs[c] != pushed[-1]:
+                return False
+            pushed.pop()
+        return not pushed
