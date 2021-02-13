b3740121a397e1e09cd1359a1219b9cda9c20933
Guan <33585352+GuanYangCLU@users.noreply.github.com>
2021-02-13 00:37:38 -0800

    Create 1041_Robot_Bounded_In_Circle.py

---
+++ LeetCode/OA/Amazon/1041_Robot_Bounded_In_Circle.py
@@ -0,0 +1,21 @@
+# 2021 Jan Amazon OA
+class Solution:
+    def isRobotBounded(self, instructions: str) -> bool:
+        if not instructions:
+            return True
+        # n: 0, e: 1, s: 2, w: 3
+        state = { 0: 0, 1: 0, 2: 0, 3: 0 }
+        direction = 0
+        turns = 1
+        while turns <= 4:
+            for c in instructions:
+                if c == 'G':
+                    state[direction] += 1
+                if c == 'L':
+                    direction = (direction + 3) % 4
+                if c == 'R':
+                    direction = (direction + 1) % 4
+            if state[0] == state[2] and state[1] == state[3]:
+                return True
+            turns += 1
+        return False
