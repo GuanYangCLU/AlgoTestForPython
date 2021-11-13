22d6a5b70525a82025d6a619399ae3eb419b8fd3
Guan <33585352+GuanYangCLU@users.noreply.github.com>
2021-11-13 12:59:36 -0800

    Update 0777_Swap_Adjacent_in_LR_String.py

--- LeetCode/twoPointer/0777_Swap_Adjacent_in_LR_String.py
+++ LeetCode/twoPointer/0777_Swap_Adjacent_in_LR_String.py
@@ -1,3 +1,31 @@
+# Better answer in LC
+class Solution(object):
+    def canTransform(self, start, end):
+        """
+        :type start: str
+        :type end: str
+        :rtype: bool
+        """
+        if len(start) != len(end): return False
+        
+        A = [(s, idx) for idx, s in enumerate(start) if s == 'L' or s == 'R']
+        B = [(e, idx) for idx, e in enumerate(end) if e == 'L' or e == 'R']
+        if len(A) != len(B): return False
+        
+        for (s, i), (e, j) in zip(A, B):
+            # LR交错了 永不可能
+            if s != e: return False
+            # 第一个L的位置在start里比在end里更左 由于L只能左移 end不可能有L可以接受这个位置
+            if s == 'L':
+                if i < j:
+                    return False
+            if s == 'R':
+                if i > j:
+                    return False
+            
+        return True
+
+# fix 9chapter wrong answer
 class Solution:
     def canTransform(self, start: str, end: str) -> bool:
         l = 0
