4620ed6ae6cecbcba36f88735f10d0f01452b4e3
Guan <33585352+GuanYangCLU@users.noreply.github.com>
2021-11-20 16:34:09 -0800

    Create 1650_Lowest_Common_Ancestor_of_a_Binary_Tree_III.py

---
+++ LeetCode/dfs/1650_Lowest_Common_Ancestor_of_a_Binary_Tree_III.py
@@ -0,0 +1,22 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+"""
+
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        # pop up p and record path node, then pop up q and check in path if visited
+        visited = set()
+        while p.parent:
+            visited.add(p)
+            p = p.parent
+        while q.parent:
+            if q in visited:
+                return q
+            q = q.parent
+        return q
