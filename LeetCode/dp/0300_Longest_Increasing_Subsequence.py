e70f5679108bcb330a47f71e1bb0509426a83644
Guan <33585352+GuanYangCLU@users.noreply.github.com>
2021-11-21 23:21:34 -0800

    Update 0300_Longest_Increasing_Subsequence.py

--- LeetCode/dp/0300_Longest_Increasing_Subsequence.py
+++ LeetCode/dp/0300_Longest_Increasing_Subsequence.py
@@ -1,3 +1,29 @@
+# https://www.jiuzhang.com/problem/longest-increasing-subsequence/
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        # dp stores the min value of the last num of LIS with length i
+        dp = [float('inf')] * (len(nums) + 1)
+        dp[0] = -float('inf')
+        res = 0
+        for n in nums:
+            index = self.first_gte(dp, n)
+            dp[index] = n
+            res = max(res, index)
+        return res
+    
+    # nums is dp list, not the input nums here
+    def first_gte(self, nums, target):
+        start, end = 0, len(nums)
+        while start + 1 < end:
+            mid = (start + end) // 2
+            if nums[mid] < target:
+                start = mid
+                continue
+            if nums[mid] >= target:
+                end = mid
+        return start if nums[start] >= target else end
+
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         # store: 2nd len with min last, 1st len
