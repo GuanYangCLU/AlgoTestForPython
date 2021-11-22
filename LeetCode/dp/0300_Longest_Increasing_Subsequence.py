a84ce9786ec7f9ad3ed2459d6b7a8cd7fe2932c5
Guan <33585352+GuanYangCLU@users.noreply.github.com>
2021-11-21 23:26:40 -0800

    Update 0300_Longest_Increasing_Subsequence.py

--- LeetCode/dp/0300_Longest_Increasing_Subsequence.py
+++ LeetCode/dp/0300_Longest_Increasing_Subsequence.py
@@ -24,6 +24,29 @@ class Solution:
                 end = mid
         return start if nums[start] >= target else end
 
+# O(n^2) easy read:
+class Solution:
+    """
+    @param nums: The integer array
+    @return: The length of LIS (longest increasing subsequence)
+    """
+    def longestIncreasingSubsequence(self, nums):
+        if nums is None or not nums:
+            return 0
+            
+        # state: dp[i] 表示以第 i 个数结尾的 LIS 的长度
+        # initialization: dp[0..n-1] = 1
+        dp = [1] * len(nums)
+        
+        # function: dp[i] = max(dp[j] + 1), j < i && nums[j] < nums[i]
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        
+        # answer, 任意一个位置都可能是 LIS 的结尾  
+        return max(dp)
+    
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         # store: 2nd len with min last, 1st len
